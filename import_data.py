020d199767756ff042c769a63d45b54bb4d869ef
s22403MB <127020826+s22403MB@users.noreply.github.com>
2025-01-31 19:33:35 +0100

    Update comments import_data.py

--- import_data.py
+++ import_data.py
@@ -2,18 +2,18 @@ import joblib
 import kagglehub
 import pandas as pd
 
-
+# Metoda do importu danych z kaggle
 def import_df():
     path = kagglehub.dataset_download("sahistapatel96/bankadditionalfullcsv")
     df = pd.read_csv(path + '/bank-additional-full.csv', sep=';', na_values='unknown')
     return df
 
-
+# Metoda do importu danych lokalnych
 def import_df_local(path):
     df = pd.read_csv(path)
     return df
 
-
+# Metoda do importu danych lokalnych przy użyciu jobliba
 def import_joblib(path):
     model = joblib.load(path)
     return model
